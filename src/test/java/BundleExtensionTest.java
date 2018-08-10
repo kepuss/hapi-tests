3a2a3351386226c94ad96a177e8a8d696c38c57d
michal <michal@tyde.com>
2018-08-10 10:52:25 +1000

    init

--- src/test/java/BundleExtensionTest.java
+++ src/test/java/BundleExtensionTest.java
@@ -1,34 +1,23 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.FhirVersionEnum;
-import ca.uhn.fhir.context.RuntimeResourceDefinition;
-import ca.uhn.fhir.context.support.IContextValidationSupport;
-import ca.uhn.fhir.fluentpath.IFluentPath;
-import ca.uhn.fhir.model.api.IFhirVersion;
 import ca.uhn.fhir.parser.IParserErrorHandler;
 import ca.uhn.fhir.parser.JsonParser;
-import ca.uhn.fhir.rest.api.IVersionSpecificBundleFactory;
 import com.google.common.collect.Sets;
 import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
 import com.sun.org.apache.xml.internal.security.utils.Base64;
 import org.apache.commons.lang3.SerializationUtils;
 import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.instance.model.api.IBase;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.instance.model.api.IIdType;
-import org.hl7.fhir.instance.model.api.IPrimitiveType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Date;
-
-import static org.apache.commons.lang3.SerializationUtils.deserialize;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 
